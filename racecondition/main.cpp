92215c999314eaae8c11c3cf99a74b80216fb11f
catalyst <39456203+catalyst2001@users.noreply.github.com>
2024-04-11 02:37:20 +0300

    lab 1

---
+++ racecondition/main.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <stdio.h>
+
+#define NUM_THREADS MAXIMUM_WAIT_OBJECTS
+
+long global_var;
+
+#define THREAD_SAFE
+
+DWORD WINAPI thread_proc(LPVOID p_param)
+{
+#ifdef THREAD_SAFE
+  InterlockedIncrement(&global_var);
+#else
+  global_var++; // no-atomic increment for demonstrate race condition
+
+#endif
+  //printf("value incremented=%d  from thread %zd\n", global_var, (size_t)p_param);
+  //printf_s("!!!!\n");
+  return 0;
+}
+
+#define CNT(x) (sizeof(x) / sizeof(x[0]))
+
+int main()
+{
+  size_t i, num_tests;
+  const size_t num_threads = NUM_THREADS;
+  HANDLE h_threads[num_threads];
+
+  while (1) {
+    printf("type num tests: ");
+    scanf_s("%zd", &num_tests);
+    for (i = 0; i < num_tests; i++) {
+      InterlockedExchange(&global_var, 0); //reset var value
+      for (size_t j = 0; j < num_threads; j++)
+        h_threads[j] = CreateThread(NULL, 0, &thread_proc, (LPVOID)j, 0, NULL);
+
+      printf("%zd threads created (pass %zd)\n", num_threads, i);
+      if (WaitForMultipleObjects((DWORD)num_threads, h_threads, TRUE, INFINITE) == WAIT_FAILED) {
+        printf("error: %d\n", GetLastError());
+        return 0;
+      }
+
+      if (NUM_THREADS != global_var) {
+        printf("race condition!!  executed %d threads but the value is %d\n", NUM_THREADS, global_var);
+        break;
+      }
+    }
+    printf("[ %s ] tests passed %zd/%zd ---\n", (i == num_tests) ? "PASSED" : "NOT PASSED", i, num_tests);
+  }
+  return 0;
+}
