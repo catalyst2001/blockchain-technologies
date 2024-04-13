494e447c3a869825e93900e7da0b377699fa104d
catalyst <39456203+catalyst2001@users.noreply.github.com>
2024-04-14 01:27:08 +0300

    update

---
+++ deadlock/deadlock.cpp
@@ -0,0 +1,29 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include <time.h>
+
+volatile DWORD WINAPI thread_proc(LPVOID p_param)
+{
+  printf("thread started\n");
+  while (1) {
+    printf("hello world!\n");
+  }
+  return 0;
+}
+
+int main()
+{
+  HANDLE h_thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&thread_proc, NULL, 0, NULL);
+
+  printf(
+    "thread started and call function time()\n"
+    "press any key to terminate thread:\n"
+  );
+
+  Sleep(100);
+  printf("call next printf() for demonstrate deadlock\n");
+  SuspendThread(h_thread);
+  //TerminateThread(h_thread, 1);
+  printf("*** this text will never come out ***\n");
+  return 0;
+}
---
+++ openmp_slau_solve/main.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <omp.h>
+
+// Функция для вывода матрицы
+void printMatrix(std::vector<std::vector<double>>& matrix) {
+  for (int i = 0; i < matrix.size(); ++i) {
+    for (int j = 0; j < matrix[i].size(); ++j) {
+      std::cout << matrix[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+// Функция для выполнения прямого хода метода Гаусса
+void forwardElimination(std::vector<std::vector<double>>& matrix, int n) {
+  for (int k = 0; k < n - 1; ++k) {
+#pragma omp parallel for
+    for (int i = k + 1; i < n; ++i) {
+      double factor = matrix[i][k] / matrix[k][k];
+      for (int j = k; j < n + 1; ++j) {
+        matrix[i][j] -= factor * matrix[k][j];
+      }
+    }
+  }
+}
+
+// Функция для выполнения обратного хода метода Гаусса
+std::vector<double> backSubstitution(std::vector<std::vector<double>>& matrix, int n) {
+  std::vector<double> solution(n);
+  solution[n - 1] = matrix[n - 1][n] / matrix[n - 1][n - 1];
+  for (int i = n - 2; i >= 0; --i) {
+    double sum = 0.0;
+#pragma omp parallel for reduction(+:sum)
+    for (int j = i + 1; j < n; ++j) {
+      sum += matrix[i][j] * solution[j];
+    }
+    solution[i] = (matrix[i][n] - sum) / matrix[i][i];
+  }
+  return solution;
+}
+
+int main() {
+  // Пример системы линейных уравнений:
+  // 2x + 3y - z = 1
+  // 4x + y + 2z = 3
+  // x + 2y - 3z = 4
+
+  std::vector<std::vector<double>> matrix = {
+      {2, 3, -1, 1},
+      {4, 1, 2, 3},
+      {4, 1, 2, 3},
+      {1, 2, -3, 4}
+  };
+
+  int n = matrix.size();
+
+  std::cout << "Original matrix:" << std::endl;
+  printMatrix(matrix);
+
+  forwardElimination(matrix, n);
+
+  std::cout << "\nMatrix after forward elimination:" << std::endl;
+  printMatrix(matrix);
+
+  std::vector<double> solution = backSubstitution(matrix, n);
+
+  std::cout << "\nSolution:" << std::endl;
+  for (int i = 0; i < n; ++i) {
+    std::cout << "x" << i + 1 << " = " << solution[i] << std::endl;
+  }
+
+  return 0;
+}
